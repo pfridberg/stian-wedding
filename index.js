df612dbe6241b8cdfa2a5289a762c8141b2aa549
Patrik Fridberg Bakken <bakken.patrik@gmail.com>
2020-07-29 18:48:37 +0200

    Title for web page and links to source code.

--- index.js
+++ index.js
@@ -11,7 +11,9 @@
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+  Source code can be found at https://github.com/neqel/stian-wedding
 */
 
 import fullpage from 'fullpage.js';
