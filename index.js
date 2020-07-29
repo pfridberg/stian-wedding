2e3026a42fd5d076ab550e219a3f822f2671fe86
Patrik Fridberg Bakken <bakken.patrik@gmail.com>
2020-07-29 18:19:52 +0200

    Add GNU GPL license v3.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+/* This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+*/
+
 import fullpage from 'fullpage.js';
 import 'fullpage.js/dist/fullpage.extensions.min.js'
 import 'fullpage.js/dist/fullpage.css'
@@ -17,5 +31,6 @@ new fullpage('#fullpage', {
     sectionsColor: autumnMist,
     slidesNavigation: true,
     navigation: true,
+    controlArrows: false,
     normalScrollElements: "#map-hotell, #map-kirke"
 });
